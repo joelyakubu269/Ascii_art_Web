cf7c2c3851e4ac0a91d15315d34088e7f34c4811
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-07 19:08:23 +0100

    creating the backend files for the server

--- ascii-web/handler.go
+++ ascii-web/handler.go
@@ -4,16 +4,8 @@ import "net/http"
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	text := r.FormValue("text")
-	ValidateInput(text)
 	banner := r.FormValue("banner")
-	allowed := map[string]bool{
-		"standard.txt":   true,
-		"shadow.txt":     true,
-		"thinkertoy.txt": true,
-	}
-	if allowed[banner] {
-		LoadBanner(banner)
-	}
+
 	data := pageData{
 		Input: text,
 	}
