d862f326d5f0fb406418628e61e9113ea8f2c553
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-07 19:07:39 +0100

    creating the backend files for the server

--- ascii-web/service.go
+++ ascii-web/service.go
@@ -2,29 +2,26 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-func transformText(text string) (string, error) {
-	// if len(os.Args) < 2 || len(os.Args) > 3 {
-	// 	fmt.Println("Usage: go run . <text> [banner file]")
-	// 	os.Exit(1)
-	// }
-	rune, err := ValidateInput(text)
+func transformText(text, bannerFile string) (string, error) {
+	r, err := ValidateInput(text)
 	if err != nil {
-		fmt.Printf("%c is not a valid ascii character\n", rune)
-		os.Exit(1)
-	}
-	input := text
-	banner := "standard.txt"
-	if len(os.Args) == 3 {
-		banner = os.Args[2]
+		return "", fmt.Errorf("%c is not a valid ascii character", r)
 	}
 
-	charMap, err := LoadBanner(banner)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	allowed := map[string]bool{
+		"standard.txt":   true,
+		"shadow.txt":     true,
+		"thinkertoy.txt": true,
 	}
-	//fmt.Print(GenerateArt(input, charMap))
+	if !allowed[bannerFile] {
+		return "", fmt.Errorf("invalid banner selected")
+	}
+	charMap, err := LoadBanner(bannerFile)
+	if err != nil {
+		return "", err
+	}
+
+	return GenerateArt(text, charMap), nil
 }
