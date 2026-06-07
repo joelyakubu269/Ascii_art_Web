97499119d76377dd6bc17afb65ea83df0481a65b
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-07 19:17:24 +0100

    creating the backend files for the server

--- ascii-web/handler.go
+++ ascii-web/handler.go
@@ -1,10 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseGlob("templates/*.html"))
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	text := r.FormValue("text")
 	banner := r.FormValue("banner")
+	result, err := transformText(text, banner)
 
 	data := pageData{
 		Input: text,
