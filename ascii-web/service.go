5a3e5a62cb743de042002deceb6f029fa97d047a
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-08 09:26:18 +0100

    trying  to export modules from my artgen

--- ascii-web/service.go
+++ ascii-web/service.go
@@ -3,6 +3,7 @@ package main
 import (
 	"asciiartweb/artgen"
 	"fmt"
+	"net/http"
 )
 
 func transformText(text, bannerFile string) (string, error) {
@@ -10,14 +11,15 @@ func transformText(text, bannerFile string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("%c is not a valid ascii character", r)
 	}
-
+	var ErrInvalidBanner = errors.New("invalid banner selected")
+	var ErrBannerNotFound = errors.New("banner not found")
 	allowed := map[string]bool{
 		"standard.txt":   true,
 		"shadow.txt":     true,
 		"thinkertoy.txt": true,
 	}
 	if !allowed[bannerFile] {
-		return "", fmt.Errorf("invalid banner selected")
+		return "",
 	}
 	charMap, err := artgen.LoadBanner(bannerFile)
 	if err != nil {
