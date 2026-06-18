d657a15c73e50c73dc9170fd384c154251a83cc8
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-18 18:03:52 +0100

    refactoring my code

--- artgen/generate.go
+++ artgen/generate.go
@@ -4,44 +4,28 @@ import (
 	"strings"
 )
 
-func GenerateArt(input string, banner map[rune][]string) string {
+func GenerateArt(input string, banner map[rune][]string) (string, error) {
 	if input == "" {
-		return ""
+		return "", nil
 	}
-
-	parts := SplitInput(input)
+	parts := split(input)
 
 	var result strings.Builder
-
 	for i, part := range parts {
 		if part == "" {
 			if i < len(parts)-1 {
 				result.WriteString("\n")
-
-			} else if part == "" && i == len(parts)-1 && parts[i-1] != "" {
-				result.WriteString(strings.Repeat("\n", 1)) // this line is used to reduce repetition and improve professionalism.
-				/*
-					just like this:
-					result.WriteString("\n")
-					result.WriteString("\n")
-					result.WriteString("\n")
-					result.WriteString("\n")
-					result.WriteString("\n")
-					result.WriteString("\n")
-					result.WriteString("\n")
-					result.WriteString("\n")
-
-					so instead of writing it this way, you can just write it on a single line with less code just like how it is used here.
-				*/
 			}
-
 		} else {
-			rows := RenderLine(part, banner)
-			for _, row := range rows {
-				result.WriteString(row)
+			n, err := RenderLine(part, banner)
+			if err != nil {
+				return "", err
+			}
+			for _, r := range n {
+				result.WriteString(r)
 				result.WriteString("\n")
 			}
 		}
 	}
-	return result.String()
+	return result.String(), nil
 }
--- artgen/render.go
+++ artgen/render.go
@@ -1,17 +1,24 @@
 package artgen
 
 import (
+	"fmt"
 	"strings"
 )
 
-func RenderLine(text string, banner map[rune][]string) []string {
+func RenderLine(text string, banner map[rune][]string) ([]string, error) {
 	result := make([]string, 8)
 	for row := 0; row < 8; row++ {
 		var builder strings.Builder
 		for _, char := range text {
-			builder.WriteString(banner[char][row])
+			val, ok := banner[char]
+			if ok {
+				builder.WriteString(val[row])
+			} else {
+				return nil, fmt.Errorf("character not found")
+			}
+
 		}
 		result[row] = builder.String()
 	}
-	return result
+	return result, nil
 }
