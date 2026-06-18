32b53212c2eefc260b5027053263860dd01ce415
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-18 18:30:05 +0100

    refactoring my code

--- ascii-web/main.go
+++ ascii-web/main.go
@@ -1,16 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
-func maian() {
-	http.HandleFunc("GET /ascii-art", handler)
-	http.HandleFunc("POST/ascii-art", handler)
+func main() {
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	http.HandleFunc("/ascii", handlers)
+	http.ListenAndServe(":8080", nil)
 }
